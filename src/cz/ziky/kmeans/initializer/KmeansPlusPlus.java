00ccd0c6d513d5886cb5e275ace10e9c30f042ed
ziky <zikara4@seznam.cz>
2015-11-20 16:50:59 +0100

    improved algorithm
    added variable that holds last minimum, so there is no need to compute
    every distance for each new centroid

--- src/cz/ziky/kmeans/initializer/KmeansPlusPlus.java
+++ src/cz/ziky/kmeans/initializer/KmeansPlusPlus.java
@@ -1,27 +1,34 @@
 package cz.ziky.kmeans.initializer;
 
+import java.util.Arrays;
 import java.util.Random;
 
 import cz.ziky.kmeans.program.Vector;
 
 public class KmeansPlusPlus implements KMeansInitializer {
-	private Vector[] centroids;
+	private Vector[] centroids; //selected centroids
+	private double[] di;		//minimum distance for each vector (from all centroid distances)
 	
 	@Override
 	public Vector[] initCentroids(Vector[] data, int k) {
 		centroids = new Vector[k];
+		di = new double[data.length];
+		Arrays.fill(di, Double.MAX_VALUE);
 		
-		Random random = new Random();
+		Random random = new Random(444);
+		//first centroid selected randomly
 		centroids[0] = data[random.nextInt(data.length)];
 		
-		double[] di = new double[data.length];
+		//selects remaining k-1 centroids
 		for (int j = 1; j < k; j++) {
 			double sum = 0;
+			//finds minimum distance for each vector
 			for (int i = 0; i < data.length; i++) {
-				di[i] = findMinDistance(j, data[i]);
+				findMinDistance(i, j, data[i]);
 				sum += di[i];
 			}
-			sum = sum * random.nextDouble(); 
+			sum = sum * random.nextDouble();
+			//select next centroid with probability proportional to D(x)^2
 			for (int i = 0; i < data.length; i++) {
 				sum -= di[i];
 				if (sum > 0) {
@@ -35,17 +42,16 @@ public class KmeansPlusPlus implements KMeansInitializer {
 		return centroids;
 	}
 	
-	private double findMinDistance(int count, Vector vector) {
-		double nearestDistance = countSqrDistance(centroids[0], vector);
-		for (int i = 1; i < count; i++) {
-			double distance = countSqrDistance(centroids[i], vector);
-			if (distance < nearestDistance) {
-				nearestDistance = distance;
-			}
+	//compares last minimum with new distance between vector and last inserted centroid	
+	private void findMinDistance(int i, int count, Vector vector) {
+		//distance between vector and last inserted centroid
+		double distance = countSqrDistance(centroids[count - 1], vector);
+		if (distance < di[i]) {
+			di[i] = distance;
 		}
-		return nearestDistance * nearestDistance;
 	}
 
+	//counts distance (Euclidean distance withnout sqrt)
 	private double countSqrDistance(Vector vector, Vector centroid) {
 		double sqrDistance = 0;
 		for (int i = 0; i < vector.dimension(); i++) {
@@ -53,5 +59,4 @@ public class KmeansPlusPlus implements KMeansInitializer {
 		}
 		return sqrDistance;
 	}
-	
 }
